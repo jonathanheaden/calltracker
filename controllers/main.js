5919149fd976d3770b2b2ae7c63998c7e73d1aa5
Jonathan Headen <jonathan.headen@outlook.com>
2017-08-03 17:48:49 +1000

    implement create new contact

--- controllers/main.js
+++ controllers/main.js
@@ -1,52 +1,66 @@
 var ctrlShared = require('./shared');
 contactdb = ctrlShared.contact;
 
-var landing = function (req, res) {   
+var landing = function (req, res) {
     ctrlShared.sendJsonResponse(res, 201, {
         'message': 'home page'
     })
 }
 
 var allContacts = function (req, res) {
-  contactdb
-    .find()
-    .exec(function (err, contact) {
-      if (!contact) {
-        ctrlShared.sendJsonResponse(res, 404, {
-          message: "no contacts found"
+    contactdb
+        .find()
+        .exec(function (err, contact) {
+            if (!contact) {
+                ctrlShared.sendJsonResponse(res, 404, {
+                    message: "no contacts found"
+                });
+                return;
+            } else if (err) {
+                ctrlShared.sendJsonResponse(res, 404, err);
+                return;
+            }
+            ctrlShared.sendJsonResponse(res, 200, contact);
         });
-        return;
-      } else if (err) {
-        ctrlShared.sendJsonResponse(res, 404, err);
-        return;
-      }
-      ctrlShared.sendJsonResponse(res, 200, contact);
-    });
 }
 
 var newContact = function (req, res) {
-    
-    ctrlShared.sendJsonResponse(res, 201, {
-        'message': 'new contact'
-    })
+    // post contacts
+    contactdb
+        .create({
+            name: req.body.name,
+            number: req.body.number,
+            address: req.body.address,
+            createdAt: new Date(),
+            description: req.body.description,
+            trade: req.body.trade,
+            email: req.body.email,
+            calls: []
+        }, function (err, contact) {
+            if (err) {
+                ctrlShared.sendJsonResponse(res, 400, err);
+            } else {
+                ctrlShared.sendJsonResponse(res, 201, contact);
+            }
+        });
 }
 
 var readContactCalls = function (req, res) {
-    
+
     ctrlShared.sendJsonResponse(res, 201, {
         'message': 'read contact'
     })
 }
 
 var newContactCall = function (req, res) {
-    
+
     ctrlShared.sendJsonResponse(res, 201, {
         'message': 'new call'
     })
 }
 
 var updateContact = function (req, res) {
-    
+
     ctrlShared.sendJsonResponse(res, 201, {
         'message': 'update contact'
     })
