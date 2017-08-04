e774a7f697f838cf86f3e959162327238c7c87e1
Jonathan Headen <jonathan.headen@outlook.com>
2017-08-04 17:46:57 +1000

    display contacts in the client

--- public/client/app.js
+++ public/client/app.js
@@ -1,11 +1,38 @@
 Vue.use(VueRouter)
 
 const Home = {
-    template:`
+    template: `
     <div class="user">
     <h1>Call Trackr</h1>
+    <ul>
+    <li v-for="contact in contacts">{{contact.name}}</li>
+    </ul>
     </div>
-    `
+    `,
+    data: function () {
+        return {
+            
+        }
+    },
+    computed: {
+        contacts(){
+            return store.state.contacts
+        }
+    },
+    methods: {
+        refreshContacts() {
+            axios.get(store.state.siteUrl + 'api/contacts')
+                .then(response => {
+                    store.commit('setContacts', response.data)
+                })
+                .catch(error => {
+                    console.log('There was an error: ' + error.message)
+                })
+        }
+    },
+    created: function () {
+        this.refreshContacts();
+    }
 }
 
 const router = new VueRouter({
@@ -16,10 +43,23 @@ const router = new VueRouter({
     mode: 'history'
 })
 
+const store = new Vuex.Store({
+    state: {
+        contacts: [],
+        siteUrl: this.document.URL.split('?')[0]
+    },
+    mutations: {
+        setContacts(state, data) {
+            state.contacts = data
+        }
+    }
+})
+
 new Vue({
     router,
+    store,
     el: '#app',
     data: {
-       
+
     }
 })
